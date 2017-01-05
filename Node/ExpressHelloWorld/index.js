d924268dfbc360949b91d1a3fcfe01641e576fe3
Sen Lin <slin@jhu.edu>
2017-01-04 17:16:29 -0800

    added an echo method.

--- Node/ExpressHelloWorld/index.js
+++ Node/ExpressHelloWorld/index.js
@@ -4,8 +4,17 @@ var app = express();
 app.use(express.static('static'));
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
-})
+    // Send static file
+    res.sendFile( __dirname + 'static/index.html');
+});
+
+app.get('/echo', function(req, res){
+    console.log(req.query);
+    response = {
+        message: req.query.message
+    };
+    res.send(response);
+});
 
 var server = app.listen(8081, function () {
     console.log(server.address());

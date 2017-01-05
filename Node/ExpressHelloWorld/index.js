ffbbe81948696d498b696f3ddb036b3c506f084d
Sen Lin <slin@jhu.edu>
2017-01-04 18:08:14 -0800

    Studied file uploading

--- Node/ExpressHelloWorld/index.js
+++ Node/ExpressHelloWorld/index.js
@@ -3,6 +3,12 @@ var app = express();
 
 app.use(express.static('static'));
 
+var bodyParser = require('body-parser');
+var multer  = require('multer');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(multer({dest:'static/upload/'}).any());
+
+
 app.get('/', function (req, res) {
     // Send static file
     res.sendFile( __dirname + 'static/index.html');
@@ -10,11 +16,18 @@ app.get('/', function (req, res) {
 
 app.get('/echo', function(req, res){
     console.log(req.query);
-    response = {
+    var response = {
         message: req.query.message
     };
     res.send(response);
 });
+
+app.post('/upload', function (req, res) {
+    var files = req.files;
+    for(var i = 0; i < files.length; i++){
+        console.log(files[i]);
+    }
+})
 
 var server = app.listen(8081, function () {
     console.log(server.address());

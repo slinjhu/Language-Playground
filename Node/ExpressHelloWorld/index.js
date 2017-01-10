93fb4e398d43cc2295728d11c9c14acd991b71de
Sen Lin <slin@jhu.edu>
2017-01-10 13:49:35 -0500

    Changed to arrow function

--- Node/ExpressHelloWorld/index.js
+++ Node/ExpressHelloWorld/index.js
@@ -9,12 +9,12 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(multer({dest:'static/upload/'}).any());
 
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     // Send static file
     res.sendFile( __dirname + 'static/index.html');
 });
 
-app.get('/echo', function(req, res){
+app.get('/echo', (req, res) => {
     console.log(req.query);
     var response = {
         message: req.query.message
@@ -22,13 +22,13 @@ app.get('/echo', function(req, res){
     res.send(response);
 });
 
-app.post('/upload', function (req, res) {
+app.post('/upload', (req, res) => {
     var files = req.files;
     for(var i = 0; i < files.length; i++){
         console.log(files[i]);
     }
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(8081, () => {
     console.log(server.address());
 })

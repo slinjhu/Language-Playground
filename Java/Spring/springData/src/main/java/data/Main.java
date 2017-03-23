648ae78a3b3855cf79d01fb23c3122a5449e858a
Sen Lin <slinjhu@gmail.com>
2017-03-23 13:50:10 -0700

    Added a few more examples

--- Java/Spring/springData/src/main/java/data/Main.java
+++ Java/Spring/springData/src/main/java/data/Main.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.Data;
 import javax.persistence.*;
@@ -31,12 +32,15 @@ class Book {
     private String title;
 
     private String author;
+    private int year;
 }
 
 
 @Repository
 interface BookRepository extends CrudRepository<Book, Long> {
     Book findById(long id);
+    Iterable<Book> findByAuthor(String author);
+    Iterable<Book> findByAuthorAndYear(String author, int year);
 }
 
 @RestController
@@ -44,9 +48,22 @@ class Controller {
     @Autowired
     private BookRepository _repository;
 
-    @GetMapping("/book/{id}")
-    public Book findBook(@PathVariable("id") long id){
+    @GetMapping("/id/{id}")
+    public Book findBookById(@PathVariable("id") long id){
         return _repository.findById(id);
+    }
+
+    @GetMapping("/author/{author}")
+    public Iterable<Book> findBookByAuthor(@PathVariable("author") String author){
+        return _repository.findByAuthor(author);
+    }
+
+    @GetMapping("/find")
+    public Iterable<Book> findBookByAuthorAndYear(
+        @RequestParam("author") String author,
+        @RequestParam("year") int year
+    ){
+        return _repository.findByAuthorAndYear(author, year);
     }
 
     @GetMapping("/allbooks")
--- Java/Spring/springData/src/main/resources/data.sql
+++ Java/Spring/springData/src/main/resources/data.sql
@@ -1,3 +1,4 @@
-INSERT INTO book (title, author) VALUES ('Introduction to Algorithms', 'Thomas H. Cormen');
-INSERT INTO book (title, author) VALUES ('Effective Java (2nd Edition)', 'Joshua Bloch');
-INSERT INTO book (title, author) VALUES ('Refactoring', 'Martin Fowler');
+INSERT INTO book (title, author, year) VALUES ('Introduction to Algorithms', 'Cormen', 1990);
+INSERT INTO book (title, author, year) VALUES ('Effective Java (2nd Edition)', 'Bloch', 2001);
+INSERT INTO book (title, author, year) VALUES ('Refactoring', 'Fowler', 1999);
+INSERT INTO book (title, author, year) VALUES ('UML distilled', 'Fowler', 1997);

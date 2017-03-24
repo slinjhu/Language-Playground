af296c160778a6adc555a01667ce20ed62b95a51
Sen Lin <slinjhu@gmail.com>
2017-03-24 12:53:22 -0700

    Moved to an indivisual repo

--- Java/Spring/springData/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO book (title, author, year) VALUES ('Introduction to Algorithms', 'Cormen', 1990);
-INSERT INTO book (title, author, year) VALUES ('Effective Java (2nd Edition)', 'Bloch', 2001);
-INSERT INTO book (title, author, year) VALUES ('Refactoring', 'Fowler', 1999);
-INSERT INTO book (title, author, year) VALUES ('UML distilled', 'Fowler', 1997);
